74e0c5ba87689db920dc8c5484d8f9e284b021ae
Mai Luu <luumai231193@gmail.com>
2021-03-16 15:59:07 -0700

    Place Order Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import {BrowserRouter, Link, Route} from 'react-router-dom'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
@@ -13,10 +14,10 @@ import SigninScreen from './screens/SigninScreen';
 function App() {
 
   const cart = useSelector(state => state.cart);
-  const {cartItems} = cart;
+  const { cartItems } = cart;
   //get user info
   const userSignin = useSelector((state => state.userSignin));
-  const {userInfo} = userSignin;
+  const { userInfo } = userSignin;
 
   const dispatch = useDispatch();
 
@@ -28,36 +29,36 @@ function App() {
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
-            <div>
-                <Link className="brand" to="/">MTStore</Link>
-            </div>
-            <div>
-                <Link to="/cart">
-                Cart
+          <div>
+            <Link className="brand" to="/">MTStore</Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
                 {/* Contional Rendering Section */}
-                {cartItems.length > 0 && (
-                  //if cartItems.length > 0 then render this
-                  <span className="badge">{cartItems.length}</span>
-                )}
-                </Link>
-                {/* Conditional rendering for Signin */}
-                {
-                  userInfo ? (
-                  //render user's name if user exists
-                  <div className="dropdown">
-                    <Link to="#">
-                      {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
-                    </Link>
-                    <ul className="dropdown-content">
-                      <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
-                    </ul>
-                  </div>
-                    
-                  ) : 
-                  //else render Sign In
-                  <Link to="/signin">Sign In</Link>
-                }
-            </div>
+              {cartItems.length > 0 && (
+                //if cartItems.length > 0 then render this
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {/* Conditional rendering for Signin */}
+            {
+              userInfo ? (
+                //render user's name if user exists
+                <div className="dropdown">
+                  <Link to="#">
+                    {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                  </Link>
+                  <ul className="dropdown-content">
+                    <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
+                  </ul>
+                </div>
+
+              ) :
+                //else render Sign In
+                <Link to="/signin">Sign In</Link>
+            }
+          </div>
         </header>
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
@@ -66,12 +67,13 @@ function App() {
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/shipping" component={ShippingAddressScreen}></Route>
           <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
       </div>
     </BrowserRouter>
-    
+
   );
 }
 
